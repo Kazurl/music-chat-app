4ef9328e60cfeb7319aa40045f42385dfd841fc4
Bernard Chiang <bczh2000@gmail.com>
2025-08-01 17:14:49 +0800

    index.js file path fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -70,9 +70,9 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+        res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"));
     });
 }
 
